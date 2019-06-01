a416b07e310430cd2f13911507eb8b9a7e6f33a6
antik9 <illarionovanton@gmail.com>
2019-06-01 13:39:13 +0300

    Add LinkShortener struct which implements Shortener interface

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-## Go Otus Repo
+## Link Shortener
+
+### Get a repo
+```bash
+>>> go get -u github.com/antik9/gootus:shortener
+```
+
+### Usage
+
+```go
+import (
+    "fmt"
+    "shortener"
+)
+
+
+func main() {
+    worker := shortner.LinkShortener{map[string]string{}, map[string]string{}}
+    fmt.Println(worker.Shorten("https://google.com")
+}
+```
---
+++ shortener.go
@@ -0,0 +1,57 @@
+package shortener
+
+import (
+	"math/rand"
+)
+
+const (
+	symbols    = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	linkLength = 6
+	prefix     = "antik.com/"
+)
+
+type Shortener interface {
+	Shorten(url string) string
+	Resolve(url string) string
+}
+
+type LinkShortener struct {
+	linksMap        map[string]string
+	linksReverseMap map[string]string
+}
+
+func randomString(length int) string {
+	bytes := make([]byte, length)
+	for i := 0; i < length; i++ {
+		bytes[i] = symbols[rand.Intn(len(symbols))]
+	}
+	return string(bytes)
+}
+
+func (shortener LinkShortener) Shorten(url string) string {
+	if shortenedLink, ok := shortener.linksMap[url]; ok {
+		return prefix + shortenedLink
+	} else {
+		for {
+			shortenedLink := randomString(linkLength)
+			if _, ok := shortener.linksReverseMap[shortenedLink]; !ok {
+				shortener.linksMap[url] = shortenedLink
+				shortener.linksReverseMap[shortenedLink] = url
+				return prefix + shortenedLink
+			}
+		}
+	}
+}
+
+func (shortener LinkShortener) Resolve(url string) string {
+	if len(url) != len(prefix)+linkLength {
+		return ""
+	} else {
+		hash := url[len(prefix):]
+		if fullUrl, ok := shortener.linksReverseMap[hash]; ok {
+			return fullUrl
+		} else {
+			return ""
+		}
+	}
+}
