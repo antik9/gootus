04b45d52f9b9863738441f1de45fd2ec7a30936c
antik9 <illarionovanton@gmail.com>
2019-06-08 01:20:00 +0300

    Top ten words library.

---
+++ top_ten.go
@@ -0,0 +1,40 @@
+package top_ten
+
+import (
+	"sort"
+	"strings"
+)
+
+type wordOccurence struct {
+	word  string
+	count int
+}
+
+func TopTen(text string) []string {
+	counter := make(map[string]int)
+
+	for _, word := range strings.Fields(text) {
+		if strings.Trim(word, " \n\t\r") != "" {
+			counter[word] = counter[word] + 1
+		}
+	}
+
+	occurences := make([]wordOccurence, len(counter))
+	topTen := make([]string, 0)
+	i := 0
+
+	for key, value := range counter {
+		occurences[i] = wordOccurence{key, value}
+		i++
+	}
+
+	sort.Slice(occurences, func(i, j int) bool {
+		return occurences[i].count > occurences[j].count
+	})
+
+	for _, occurence := range occurences[:10] {
+		topTen = append(topTen, occurence.word)
+	}
+
+	return topTen
+}
