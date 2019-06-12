0e8d075ac014ae58daf80b7a40e546888a1b028f
antik9 <illarionovanton@gmail.com>
2019-06-12 17:48:40 +0300

    Add check for slice type.

--- max.go
+++ max.go
@@ -11,9 +11,9 @@ func FindMax(_slice interface{}, less lessFunc) (interface{}, error) {
 	if _slice == nil {
 		return nil, errors.New("nil is given")
 	} else {
-		slice := reflect.ValueOf(_slice).Convert(reflect.TypeOf(_slice))
-		if slice.Len() == 0 {
-			return nil, errors.New("Empty slice")
+		slice := reflect.ValueOf(_slice)
+		if reflect.TypeOf(_slice).Kind() != reflect.Slice || slice.Len() == 0 {
+			return nil, errors.New("Provide not empty slice")
 		} else {
 			maxElementIndex := 0
 			for idx := 1; idx < slice.Len(); idx++ {
